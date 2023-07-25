79fc69b81b23c7a7a390c7da8f4a89747ca0ca12
Abdullah Zyoud <aboodzyoud.1997@gmail.com>
2023-07-25 10:06:27 +0300

    add recursion

--- Recursion/Recursion_To_Do_3.js
+++ Recursion/Recursion_To_Do_3.js
@@ -54,3 +54,48 @@ function rGCF(num1, num2) {
 }
 
 console.log(rGCF(123456, 987654));
+///////////////////////////////////////////////
+// The Tarai (Japanese: “to pass around”) function was created to profile recursive performance in various systems and languages. Unlike other functions, numbers don't get particularly large, but the amount of recursion is significant. The tarai function accepts three parameters and is defined as:
+
+// tarai(x,y,z) == y, if x <= y (otherwise see rule #2);
+// tarai(x,y,z) == tarai(tarai(x-1,y,z),tarai(y-1,z,x),tarai(z-1,x,y)).
+// Calling tarai(10,2,9) should return 9 (after recursing 4145 times!).
+
+function tarai(x, y, z) {
+    if (x <= y) {
+      return y;
+    } else {
+      return tarai(tarai(x - 1, y, z), tarai(y - 1, z, x), tarai(z - 1, x, y));
+    }
+  }
+  
+  console.log(tarai(10, 2, 9)); 
+
+  ///////////////////////////////////////////////////
+
+//   String: In-Order Subsets
+// Create strSubsets(str). Return an array with every possible in-order character subset of str.
+//  The resultant array itself need not be in any specific order – it is the subset of letters in each 
+//  string that must be in the same order as they were in the original string. Given "abc",
+//  return an array that includes ["", "c", "b", "bc", "a", "ac", "ab", "abc"] (in any order).
+
+
+  function strSubsets(str) {
+    const subsets = [];
+  
+    function generateSubsets(currentSubset, index) {
+      if (index === str.length) {
+        subsets.push(currentSubset);
+        return;
+      }
+  
+      generateSubsets(currentSubset, index + 1); 
+      generateSubsets(currentSubset + str[index], index + 1); 
+    }
+  
+    generateSubsets('', 0);
+    return subsets;
+  }
+  
+  console.log(strSubsets('abc'));
+  
